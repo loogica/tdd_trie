f6e535729b42809c2b9d7fe61ff33403b73240ae
Felipe cruz <felipecruz@loogica.net>
2013-05-07 21:09:02 -0300

    fix style issues

--- src/trie.c
+++ src/trie.c
@@ -1,9 +1,9 @@
 #include <stdlib.h>
 #include "trie.h"
 
-trie_node_t* trie_init()
+trie_node_t* trie_init (void)
 {
     trie_node_t* trie;
-    trie = malloc(sizeof(trie_node_t));
+    trie = malloc (sizeof (trie_node_t));
     return trie;
 }
